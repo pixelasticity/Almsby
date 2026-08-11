db907cfa4ca9b0255b3012347a17533b6181aa40
Alexander Bell <contact@alexanderbell.info>
2026-08-10 22:33:05 -0700

    Add Vike

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import vike from 'vike/plugin'
 
 export default defineConfig({
   plugins: [
@@ -9,6 +10,7 @@ export default defineConfig({
     // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
+    vike(),
   ],
   resolve: {
     alias: {
